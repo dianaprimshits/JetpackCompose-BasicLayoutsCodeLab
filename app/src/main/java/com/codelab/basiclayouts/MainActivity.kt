1a9c27d8eebe250d11a46cceaff875169b4cbbb7
Dziyana Prymshyts <dianaprimshits@gmail.com>
2023-07-17 17:17:00 +0200

    Added searchBar.

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,9 +21,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
@@ -35,12 +45,32 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// Step: Search bar - Modifiers
 @Composable
-fun SearchBar(
-    modifier: Modifier = Modifier
-) {
+fun MySootheApp() {
     // Implement composable here
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(text = stringResource(id = R.string.placeholder_search))
+        }
+    )
 }
 
 // Step: Align your body - Alignment
@@ -92,12 +122,6 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 // Step: Bottom navigation - Material
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
-    // Implement composable here
-}
-
-// Step: MySoothe App - Scaffold
-@Composable
-fun MySootheApp() {
     // Implement composable here
 }
 
